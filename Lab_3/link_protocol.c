84aaf8b400926ccd97c13280be0387c2e0c1b55f
Ana Margarida <anamargaridarl@gmail.com>
2019-10-12 16:50:07 +0100

    started llread

--- Lab_3/link_protocol.c
+++ Lab_3/link_protocol.c
@@ -1,2 +1,113 @@
 #include "link_protocol.h"
+#include "state_machine_frame.c"
+
+#include <unistd.h>
+
+int main()
+{
+   struct Message *message;
+    
+    llread(2,message);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+volatile int STOP=FALSE;
+
+int llread(int fd, struct Message *message)
+{
+    unsigned char buffer[256];
+    int i = 0 ;
+    int res = 0;
+    int state = 0;
+
+    while (STOP==FALSE)
+    {
+      res = read(fd,&buffer[i],1);
+      if (buffer[i] == FLAG && i != 0)  STOP=TRUE;
+      state_machine(&state, buffer[i], message);
+      i++;
+    }
+
+    
+
+    
+
+    
+}
 
--- Lab_3/link_protocol.h
+++ Lab_3/link_protocol.h
@@ -1,9 +1,11 @@
 #ifndef _LINK_PROTOCOL_H
 #define _LINK_PROTOCOL_H
 
+#include "defines.h"
+
 int llopen(int port, int flag);
 int llwrite(int fd, char *buffer, int length);
-int llread(int fd, char *buffer);
+int llread(int fd, struct Message *message);
 int llclose(int fd);
 
 
