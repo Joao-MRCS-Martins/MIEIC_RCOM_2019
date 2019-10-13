3c1963e9e0b93384d558b3d87da1ed9e1501d0c5
Filipe Pinto Reis <filipepintodosreis@gmail.com>
2019-10-13 11:04:53 +0100

    stuffiing

--- Lab_3/stuffing.c
+++ Lab_3/stuffing.c
@@ -1,14 +1,46 @@
-#define FLAG 0x7e
-#define ESCAPE 0x7d
+  #define FLAG 0x7e
+  #define ESCAPE 0x7d
 
-void stuffing(unsigned char** array){
+  #include <stdio.h>
 
-}
+  void stuffing(unsigned char* array, int size){
+    int final_size = size;
+    for(; *array != '\0'; array++){
+      if(*array == ESCAPE || *array == FLAG)
+        final_size++;
+    }
+    printf("%d\n", final_size);
 
-int main(int argc, char** argv){
-  if(argc < 2)
-    exit(1);
+    unsigned char ret_vect[final_size];
+    printf("%lu\n", sizeof(ret_vect));
 
+    unsigned char * p = ret_vect;
 
-  return 0;
-}
+    for(; *p != '\0'; p++){
+      if(*p == ESCAPE){
+        p++;
+        *p = ESCAPE ^ 0x20;
+      }
+      else if(*p == FLAG){
+        *p = ESCAPE;
+        p++;
+        *p = FLAG ^ 0x20;
+      }
+    }
+
+    
+    for(long unsigned int i = 0; i < sizeof(ret_vect); i++){
+      printf("%u\n", ret_vect[i]);
+    }
+
+  }
+
+  int main(int argc, char** argv){
+    //if(argc < 2)
+    //  exit(1);
+
+    unsigned char ar[2] = {FLAG,ESCAPE};
+    stuffing(ar, 2);
+
+    return 0;
+  }
