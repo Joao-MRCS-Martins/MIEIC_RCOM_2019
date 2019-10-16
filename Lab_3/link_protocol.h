825e0a142131d4ac88b84635862eb35e3e008aea
João Martins <up201707311@fe.up.pt>
2019-10-16 22:51:41 +0100

    missing header update

--- Lab_3/link_protocol.h
+++ Lab_3/link_protocol.h
@@ -10,10 +10,7 @@ int llread(int fd, unsigned char *packets);
 int llclose(int fd, int flag);
 
 // helper functions
-int send_SET();
-int send_DISC();
-int send_UA();
-void bcc2_calc(unsigned char *message, int length, unsigned char *bcc2);
+unsigned char *bcc2_calc(unsigned char *message, int length);
 void alarmHandler();
 
 #endif
